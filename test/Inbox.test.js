2e2e5d12611ad85f1c81aa59350eeb13888c492e
Matheus Almeida <mat.almeida@live.com>
2018-11-30 01:45:12 -0300

    refac: refactoring tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const expect = require('chai').expect;
 const ganache = require('ganache-core');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
@@ -6,32 +6,40 @@ const web3 = new Web3(ganache.provider());
 // Contracts
 const contract = require('../src/compile');
 
-let accounts;
-let inbox;
-beforeEach(async () => {
-    accounts = await web3.eth.getAccounts();
-    inbox = await new web3.eth.Contract(JSON.parse(contract.interface))
-        .deploy({
-            data: contract.bytecode,
-            arguments: ['Meu primeiro contrato']
-        })
-        .send({ from: accounts[0], gas: 1000000 });
-});
-
 describe('Inbox', () => {
+    const defaultArguments = ['Meu primeiro contrato'];
+    const defaultContract = {
+        data: contract.bytecode,
+        arguments: defaultArguments
+    };
+
+    let accounts;
+    let inbox;
+
+    const retriveMessage = async (inbox) => inbox.methods.message().call();
+
+    beforeEach(async () => {
+        accounts = await web3.eth.getAccounts();
+        inbox = await new web3.eth.Contract(JSON.parse(contract.interface))
+            .deploy(defaultContract)
+            .send({ from: accounts[0], gas: 1000000 });
+    });
+
     it('Deploy a contract', async () => {
-        assert.ok(inbox.options.address);
+        expect(inbox.options.address).to.be.ok;
     });
 
     it('Default message', async () => {
-        const msg = await inbox.methods.message().call();
-        assert.equal(msg, 'Meu primeiro contrato');
+        const retrivedMessage = await retriveMessage(inbox);
+
+        expect(retrivedMessage).to.equal(defaultArguments[0]);
     });
 
     it('Changing message', async () => {
         const newMessage = 'New Message';
         await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
-        const msg = await inbox.methods.message().call();
-        assert.equal(msg, newMessage);
+        const retrivedMessage = await retriveMessage(inbox);
+
+        expect(retrivedMessage).to.equal(newMessage);
     });
 });
