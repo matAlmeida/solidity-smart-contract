841c9ffc6e754c6c343cafccc6f28a517b4f117f
Matheus Almeida <mat.almeida@live.com>
2018-11-30 11:23:08 -0300

    reafac: changing messages

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,23 +4,25 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
 // Contracts
-const contract = require('../src/compile');
+const { interface, bytecode } = require('../src/compile');
 
 describe('Inbox', () => {
   const defaultArguments = ['Meu primeiro contrato'];
   const defaultContract = {
-    data: contract.bytecode,
+    data: bytecode,
     arguments: defaultArguments
   };
 
   let accounts;
   let inbox;
 
-  const retriveMessage = async (inbox) => inbox.methods.message().call();
+  const getMessage = async () => await inbox.methods.message().call();
+  const sendMessage = async (message) =>
+    await inbox.methods.setMessage(message).send({ from: accounts[0] });
 
-  beforeEach(async () => {
+  beforeEach('Recreate Contract', async () => {
     accounts = await web3.eth.getAccounts();
-    inbox = await new web3.eth.Contract(JSON.parse(contract.interface))
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
       .deploy(defaultContract)
       .send({ from: accounts[0], gas: 1000000 });
   });
@@ -30,15 +32,15 @@ describe('Inbox', () => {
   });
 
   it('Default message', async () => {
-    const retrivedMessage = await retriveMessage(inbox);
+    const retrivedMessage = await getMessage();
 
     expect(retrivedMessage).to.equal(defaultArguments[0]);
   });
 
   it('Changing message', async () => {
     const newMessage = 'New Message';
-    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
-    const retrivedMessage = await retriveMessage(inbox);
+    await sendMessage(newMessage);
+    const retrivedMessage = await getMessage();
 
     expect(retrivedMessage).to.equal(newMessage);
   });
